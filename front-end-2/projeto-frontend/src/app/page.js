a496b7b18f2e787dc1d3ecb2123cdea940fc3b40
Andre1753J <andreorocondo7801@gmail.com>
2024-11-01 14:22:32 -0400

    01/11/2024

---
+++ front-end-2/projeto-frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+     <h1>Meu titulo</h1>
+     <input type="text" />
+    </div>
+  );
+}
