14e760691fcbeb6004c0ba3addf6796b7ec6eebc
Andre1753J <andreorocondo7801@gmail.com>
2024-11-24 22:06:11 -0400

    terminado

--- front-end-2/projeto-frontend/src/components/Header/index.js
+++ front-end-2/projeto-frontend/src/components/Header/index.js
@@ -1,13 +1,31 @@
-import Link from "next/link"
-import styles from "./Header.module.css";
-import Image from "next/image";
+"use client";
+
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './Header.module.css';
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <header>
-            <nav>
+        <header className={styles.header}>
+            <div className={styles.navContainer}>
+                <Image
+                    src="/images/iconSenku.jpg"
+                    alt="Menu Icon"
+                    className={styles.menuIcon}
+                    onClick={toggleMenu}
+                    width={50}
+                    height={50}
+                />
+            </div>
+            <nav className={`${styles.nav} ${menuOpen ? styles.navOpen : ''}`}>
                 <ul className={styles.list}>
-                    <Image className={styles.img} src='/images/iconSenku.jpg' width={100} height={100}/>
                     <li className={styles.lit}>
                         <Link href='/'>Holmes</Link>
                     </li>
@@ -20,5 +38,5 @@ export default function Header() {
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
